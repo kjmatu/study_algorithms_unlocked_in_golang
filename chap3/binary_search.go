e128f75ff8abdea55a225960c93e85e3af2e51e7
Kenji Matsunaga <kenji.matsunaga@me.com>
2019-01-13 14:55:34 +0900

    Golangでは配列の長さを取得できるため引数を変更したbinary_searchを実装

--- chap3/binary_search.go
+++ chap3/binary_search.go
@@ -21,8 +21,29 @@ func binarySearch(searchArray []int, arraySize int, targetNumber int) int {
 	return -1
 }
 
+func binarySearchGolike(searchArray []int, targetNumber int) int {
+	startIndex := 1
+	endIndex := len(searchArray) - 1
+
+	for startIndex <= endIndex {
+		centerIndex := (startIndex + endIndex) / 2
+		if searchArray[centerIndex] == targetNumber {
+			return centerIndex
+		} else {
+			if searchArray[centerIndex] > targetNumber {
+				endIndex = centerIndex - 1
+			} else {
+				startIndex = centerIndex + 1
+			}
+		}
+	}
+	return -1
+}
+
 func main() {
 	array := []int{1, 3, 4, 5, 6, 7, 8, 9, 10, 11, 14, 16}
 	ret := binarySearch(array, len(array), 16)
+	fmt.Println(ret)
+	ret = binarySearchGolike(array, 16)
 	fmt.Println(ret)
 }
